eb3a258538673cde867abb037ba50f826f8020d4
Silence-dream <lyqsl@foxmail.com>
2025-04-29 12:02:12 +0800

    添加baseURl

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import { vitePluginReactSource } from 'react-find/vite';
 export default defineConfig({
   plugins: [react(), vitePluginReactSource()],
-
+  base: '/ImageCompression/',
   // 优化WebAssembly加载
   server: {
     headers: {
